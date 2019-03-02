3d65b465842a6d862c9606236e48a9a719b19daf
DeltaImpact <p.grachev.vik@gmail.com>
2019-03-02 13:19:55 +0300

    feat Added new auth helper.

--- src/helpers/auth-header.js
+++ src/helpers/auth-header.js
@@ -1,28 +1,38 @@
 export function authHeader() {
-    // return authorization header with jwt token
-    let user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.token) {
-        return 'Bearer ' + user.token;
-    } else {
-        return {};
-    }
+  // return authorization header with jwt token
+  let user = JSON.parse(localStorage.getItem("user"));
+  if (user && user.token) {
+    return "Bearer " + user.token;
+  } else {
+    return {};
+  }
 }
 
 export function authToken() {
-    let user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.token) {
-        return user.token;
-    } else {
-        return undefined;
-    }
+  let user = JSON.parse(localStorage.getItem("user"));
+  if (user && user.token) {
+    return user.token;
+  } else {
+    return undefined;
+  }
 }
 
 export function userNickname() {
-    let user = JSON.parse(localStorage.getItem('user'));
-    // debugger
-    if (user && user.username) {
-        return user.username;
-    } else {
-        return undefined;
-    }
+  let user = JSON.parse(localStorage.getItem("user"));
+  // debugger
+  if (user && user.username) {
+    return user.username;
+  } else {
+    return undefined;
+  }
+}
+
+export function userId() {
+  let user = JSON.parse(localStorage.getItem("user"));
+  // debugger
+  if (user && user.id) {
+    return user.id;
+  } else {
+    return undefined;
+  }
 }
